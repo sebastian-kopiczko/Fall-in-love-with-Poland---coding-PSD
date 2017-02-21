96a909315733a4d0c4c9af95140793c5e0550029
sebastian-kopiczko <sebastian.kopiczko@gmail.com>
2017-02-21 13:20:11 +0100

    files added

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+    var gulp = require('gulp');
+    var sass = require('gulp-sass');
+    var autoprefixer = require('gulp-autoprefixer');
+    var sourcemaps = require('gulp-sourcemaps');
+    var browserSync = require('browser-sync').create();
+    var imagemin = require('gulp-imagemin');
+    gulp.task('sass', function () {
+        return gulp.src('app/scss/styles.scss').pipe(sourcemaps.init()).pipe(sass().on('error', sass.logError)).pipe(sourcemaps.write()).pipe(autoprefixer()).pipe(gulp.dest('app/css')).pipe(browserSync.reload({
+            stream: true
+        }));
+    });
+    gulp.task('browserSync', function () {
+        browserSync.init({
+            server: {
+                baseDir: 'app'
+            }
+        , })
+    })
+    gulp.task('watch', ['browserSync', 'sass'], function () {
+        gulp.watch('app/scss/**/*.scss', ['sass']);
+        gulp.watch('app/*.html', browserSync.reload);
+        gulp.watch('app/js/**/*.js', browserSync.reload);
+    });
+    gulp.task('images', function () {
+        return gulp.src('app/images/**/*.+(png|jpg|svg)').pipe(imagemin()).pipe(gulp.dest('dist/images'))
+    });
